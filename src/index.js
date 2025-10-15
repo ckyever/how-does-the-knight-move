455284e0560f05311cde6280e537dc46d2d4a041
ckyever <ckyeverg@gmail.com>
2025-10-15 23:11:45 +1100

    Add chess board and initial styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./styles.css";
+
+const chessboard = document.querySelector("#chessboard");
+
+const CHESSBOARD_SIZE = 8;
+
+// Ensure bottom left corner coordinates are (0,0)
+for (let y = CHESSBOARD_SIZE - 1; y >= 0; y--) {
+  for (let x = 0; x < CHESSBOARD_SIZE; x++) {
+    const square = document.createElement("div");
+    square.classList = "square";
+
+    if ((x + y) % 2 == 0) {
+      square.classList += " black";
+    } else {
+      square.classList += " white";
+    }
+
+    square.dataset.x = x;
+    square.dataset.y = y;
+    chessboard.appendChild(square);
+  }
+}
