14104de5f28d3593b2a9cdd3383dbfa013bf64df
ckyever <ckyeverg@gmail.com>
2025-10-16 12:43:02 +1100

    Add function to convert position to chess notation string

--- src/utility.js
+++ src/utility.js
@@ -1,3 +1,8 @@
+export function convertPositionToChessNotation([x, y]) {
+  const fileLetter = String.fromCharCode("a".charCodeAt(0) + x);
+  return `${fileLetter}${y + 1}`;
+}
+
 export function delay(milliseconds) {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 }
